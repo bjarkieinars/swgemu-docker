b48572fba4ab890f71e1e97f4b118227f7ff7f6d
Bjarki Einarsson <bjarkieinars@gmail.com>
2025-02-15 10:01:32 +0000

    glowybadges

--- custom_scripts/object/tangible/terminal/terminal_character_builder.lua
+++ custom_scripts/object/tangible/terminal/terminal_character_builder.lua
@@ -56,7 +56,7 @@ object_tangible_terminal_terminal_character_builder = object_tangible_terminal_s
 
 	templateType = CHARACTERBUILDERTERMINAL,
 
-	glowyBadgeIds = {12, 14, 15, 16, 17, 19, 20, 21, 23, 30, 38, 39, 71, 105, 106, 107},
+	glowyBadgeIds = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 71, 105, 106, 107},
 
 	villageBranchUnlocks = {
 		"force_sensitive_combat_prowess_ranged_accuracy",
